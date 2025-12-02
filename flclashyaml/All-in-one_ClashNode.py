4eb2c9fd519c816ed97b711b7e9781792826fbe3
leonhsuxu <leon.hsuxu@gmail.com>
2025-12-02 13:14:42 +0800

    Update All-in-one_ClashNode.py

--- flclashyaml/All-in-one_ClashNode.py
+++ flclashyaml/All-in-one_ClashNode.py
@@ -79,7 +79,8 @@ COUNTRY_NAME_TO_CODE_MAP = {
 }
 
 JUNK_PATTERNS = re.compile(
-    r"(?:专线|IPLC|IEPL|BGP|体验|官网|倍率|x\d[\.\d]*|Rate|[\[\(【「].*?[\]\)】」]|^\s*@\w+\s*|Relay|流量)|(?:(?:[\u2460-\u2473\u2776-\u277F\u2780-\u2789]|免費|回家).*?(?=,|$))", ,
+    r"(?:专线|IPLC|IEPL|BGP|体验|官网|倍率|x\d[\.\d]*|Rate|[\[\(【「].*?[\]\)】」]|^\s*@\w+\s*|Relay|流量)"
+    r"|(?:(?:[\u2460-\u2473\u2776-\u277F\u2780-\u2789]|免費|回家).*?(?=,|$))",
     re.IGNORECASE
 )
 
