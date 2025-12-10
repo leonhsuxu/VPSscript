136e4d7693e36b2da11c8d8958775c809eb6c414
leonhsuxu <leon.hsuxu@gmail.com>
2025-12-11 01:45:03 +0800

    Update TBD-telegram_publiclink.py

--- TelegramNode/TBD-telegram_publiclink.py
+++ TelegramNode/TBD-telegram_publiclink.py
@@ -76,11 +76,11 @@ SOCKET_TIMEOUT = 3       # 套接字连接超时时间，单位为秒
 HTTP_TIMEOUT = 5         # HTTP请求超时时间，单位为秒
 # 【关键修改1】测速目标全部换成国内/Cloudflare中国节点
 TEST_URLS = [
-    # 'http://www.baidu.com/generate_204',           # 百度 204，最快最稳
-    # 'http://qq.com/generate_204',                    # 腾讯 204
-    # 'http://cp.cloudflare.com/generate_204',       # Cloudflare 中国大陆节点
-    'http://www.gstatic.com/generate_204',
-    # 'http://connectivitycheck.gstatic.com/generate_204',  # Google 204（国内也通）
+    'http://www.baidu.com/generate_204',           # 永远第1快
+    'http://qq.com/generate_204',                  # 第2快
+    'http://connect.rom.miui.com/generate_204',    # 小米官方，超稳
+    'http://connectivitycheck.platform.hicloud.com/generate_204',  # 华为官方
+    'http://captive.v2ex.com/generate_204',        # 社区良心
 ]
 
 # ==================== 测速结果_带宽筛选配置（新增） ====================
