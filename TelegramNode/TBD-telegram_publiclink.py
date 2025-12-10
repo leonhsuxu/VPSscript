80ee8a0c40a55f3813fd69a9ee1c8713301cdfde
leonhsuxu <leon.hsuxu@gmail.com>
2025-12-11 01:14:02 +0800

    Update TBD-telegram_publiclink.py

--- TelegramNode/TBD-telegram_publiclink.py
+++ TelegramNode/TBD-telegram_publiclink.py
@@ -70,8 +70,8 @@ SPEEDTEST_MODE = os.getenv('SPEEDTEST_MODE', 'tcp_first').lower()  # 默认推�
 TCP_TIMEOUT = 3.5          # 单次 TCP 连接超时时间（秒），建议 3~5
 TCP_MAX_WORKERS = 512      # TCP 测速最大并发（可以比 Clash 高很多，非常快）
 TCP_MAX_DELAY = 1000       # TCP 延迟阈值，超过此值直接丢弃（ms）
-ENABLE_TCP_LOG = False     # 默认关闭TCP日志
-ENABLE_SPEEDTEST_LOG = False  # 默认关闭 speedtest 详细日志False / True打开
+ENABLE_TCP_LOG = str_to_bool(os.getenv('ENABLE_TCP_LOG', 'false'))
+ENABLE_SPEEDTEST_LOG = str_to_bool(os.getenv('ENABLE_SPEEDTEST_LOG', 'false'))
 
 
 MAX_TEST_WORKERS = 128    # 速度测试时最大并发工作线程数，控制测试的并行度。建议64-96
